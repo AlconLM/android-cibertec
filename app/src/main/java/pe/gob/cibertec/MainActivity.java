46bd0220651fa9afc06912dd62fda9804d7527da
Alcon <lalcantata@gmail.com>
2019-02-09 19:39:52 -0500

    Layout

--- app/src/main/java/pe/gob/cibertec/MainActivity.java
+++ app/src/main/java/pe/gob/cibertec/MainActivity.java
@@ -2,13 +2,21 @@ package pe.gob.cibertec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_constraint_main);
+        Button btn = (Button) findViewById(R.id.btnMeGusta);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
 }
